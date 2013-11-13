81dd63b3f43443bcb43f893f3e41c1bab55ceb1f
Gabriel Andrade <gabiruh@gmail.com>
2013-11-13 10:15:39 -0200

    eat_treehash
    Signed-off-by: Gabriel Andrade <gabiruh@gmail.com>

--- lib/Net/Amazon/TreeHash.pm
+++ lib/Net/Amazon/TreeHash.pm
@@ -18,10 +18,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-
 package Net::Amazon::TreeHash;
-
-
 
 use strict;
 use warnings;
@@ -75,88 +72,112 @@ Does not work for 0-length files (however it's useless for Glacier).
 
 =cut
 
-
-sub new
-{
-    my ($class, %args) = @_;
-    my $self = \%args;
-    $self->{tree} = [];
-    $self->{pending} = {};
-    $self->{unit} ||= 1048576;
-    $self->{processed_size} = 0; # MB
-    bless $self, $class;
-    return $self;
+sub new {
+  my ( $class, %args ) = @_;
+  my $self = \%args;
+  $self->{tree}    = [];
+  $self->{pending} = {};
+  $self->{unit} ||= 1048576;
+  $self->{processed_size} = 0;    # MB
+  bless $self, $class;
+  return $self;
 }
 
-
-sub eat_file
-{
-	my ($self, $fh) = @_;
-	while (1) {
-		my $r = sysread($fh, my $data, $self->{unit});
-		if (!defined($r)) {
-			die;
-		} elsif ($r > 0) {
-			$self->_eat_data_one_mb(\$data);
-		} else {
-			return;
-		}
-	}
+sub eat_file {
+  my ( $self, $fh ) = @_;
+  while (1) {
+    my $r = sysread( $fh, my $data, $self->{unit} );
+    if ( !defined($r) ) {
+      die;
+    }
+    elsif ( $r > 0 ) {
+      $self->_eat_data_one_mb( \$data );
+    }
+    else {
+      return;
+    }
+  }
 }
 
-sub eat_data
-{
-	my ($self, $dataref)  = @_;
-	my $mb = $self->{unit};
-	my $n = length($$dataref);
-	my $i = 0;
-	while ($i < $n) {
-		my $part = substr($$dataref, $i, $mb);
-		$self->_eat_data_one_mb(\$part);
-		$i += $mb
-	}
+sub eat_array {
+  my ( $self, $array_ref ) = @_;
+  $self->_eat_data_one_mb( \$_ ) for @$array_ref;
 }
 
-
-sub _eat_data_one_mb
-{
-	my ($self, $dataref)  = @_;
-	$self->{tree}->[0] ||= [];
-	push @{ $self->{tree}->[0] }, { joined => 0, start => $self->{processed_size}, finish => $self->{processed_size}, hash => sha256($$dataref) };
-	$self->{processed_size}++;
+sub eat_data {
+  my ( $self, $dataref ) = @_;
+  my $mb = $self->{unit};
+  my $n  = length($$dataref);
+  my $i  = 0;
+  while ( $i < $n ) {
+    my $part = substr( $$dataref, $i, $mb );
+    $self->_eat_data_one_mb( \$part );
+    $i += $mb;
+  }
 }
 
-sub calc_tree
-{
-	my ($self)  = @_;
-	my $prev_level = 0;
-	while (scalar @{ $self->{tree}->[$prev_level] } > 1) {
-		my $curr_level = $prev_level+1;
-		$self->{tree}->[$curr_level] = [];
-		
-		my $prev_tree = $self->{tree}->[$prev_level];
-		my $curr_tree = $self->{tree}->[$curr_level];
-		my $len = scalar @$prev_tree;
-		for (my $i = 0; $i < $len; $i += 2) {
-			if ($len - $i > 1) {
-				my $a = $prev_tree->[$i];
-				my $b = $prev_tree->[$i+1];
-				push @$curr_tree, { joined => 0, start => $a->{start}, finish => $b->{finish}, hash => sha256( $a->{hash}.$b->{hash} ) };
-			} else {
-				push @$curr_tree, $prev_tree->[$i];
-			}
-		}
-		
-		$prev_level = $curr_level;
-	}
+sub _eat_data_one_mb {
+  my ( $self, $dataref ) = @_;
+  $self->{tree}->[0] ||= [];
+  push @{ $self->{tree}->[0] },
+    {
+    joined => 0,
+    start  => $self->{processed_size},
+    finish => $self->{processed_size},
+    hash   => sha256($$dataref)
+    };
+  p($self->{tree});
+  $self->{processed_size}++;
 }
 
-
-sub get_final_hash
-{
-	my ($self)  = @_;
-	return unpack('H*', $self->{tree}->[ $#{$self->{tree}} ]->[0]->{hash} );
+sub eat_treehash {
+  my ( $self, $th ) = @_;
+  $self->{tree}->[0] ||= [];
+  push @{ $self->{tree}->[0] },
+    {
+    joined => 0,
+    start  => $self->{processed_size},
+    finish => $self->{processed_size},
+    hash   => $_
+    }
+    for @$th;
+  $self->{processed_size}++;
 }
 
+sub calc_tree {
+  my ($self) = @_;
+  my $prev_level = 0;
+  while ( scalar @{ $self->{tree}->[$prev_level] } > 1 ) {
+    my $curr_level = $prev_level + 1;
+    $self->{tree}->[$curr_level] = [];
+
+    my $prev_tree = $self->{tree}->[$prev_level];
+    my $curr_tree = $self->{tree}->[$curr_level];
+    my $len       = scalar @$prev_tree;
+    for ( my $i = 0 ; $i < $len ; $i += 2 ) {
+      if ( $len - $i > 1 ) {
+        my $a = $prev_tree->[$i];
+        my $b = $prev_tree->[ $i + 1 ];
+        push @$curr_tree,
+          {
+          joined => 0,
+          start  => $a->{start},
+          finish => $b->{finish},
+          hash   => sha256( $a->{hash} . $b->{hash} )
+          };
+      }
+      else {
+        push @$curr_tree, $prev_tree->[$i];
+      }
+    }
+
+    $prev_level = $curr_level;
+  }
+}
+
+sub get_final_hash {
+  my ($self) = @_;
+  return unpack( 'H*', $self->{tree}->[ $#{ $self->{tree} } ]->[0]->{hash} );
+}
 
 1;
